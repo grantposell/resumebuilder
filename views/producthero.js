20f6709553dc552760de5434732e2387c6bdd5ef
grantposell <grantposell@gmail.com>
2022-04-07 02:03:24 -0500

    Changed the url to s3

--- views/producthero.js
+++ views/producthero.js
@@ -12,7 +12,7 @@ import Image from 'next/image'
 //       height="300px"
 //     />
 const backgroundImage =
-  'https://cvws.icloud-content.com/B/AYA_xbpdZa3yOrNwg2SapqsVwMyXAXzeiXPFwiRgaKd1ZhSrVWeirEeQ/resume.jpeg?o=AguQz7J_Ltqj_oCuyiPLI201hEs0T1vUifm1XmyBTOM1&v=1&x=3&a=CAogoMjO4KbwJA_NQH1oAyaRQvluQYEk8bgTFVtcBeZL9l4SaxDX2PiKgDAY98-vi4AwIgEAUgQVwMyXWgSirEeQaiUaAB53El-U1l7TKKNtYy_J6Zx2tLbe39M7j5ceXshHRBBMCLq6ciUySsOjMVTrg6oGu42LcrpZhXXuwo_2vyTigWKE54EbZq8ehZ-v&e=1649291290&fl=&r=d1e7ab00-a2e8-462b-9766-d10732c569c0-1&k=a_CEcZ0YjmL_jmBxzu0bDg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=45&s=bb3xTVflCD3ZFIoRMGKgZ2XP--Y&cd=i';
+  'https://raven-resumes.s3.amazonaws.com/resume.jpeg';
 
 export default function ProductHero() {
   return (
