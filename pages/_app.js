fdb42b414103a8496417e4343e2fdffa457c54fb
grantposell <grantposell@gmail.com>
2022-04-12 20:23:20 -0500

    Google Analytics is working now

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,35 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
+import Script from "next/script";
 config.autoAddCss = false
 
+
 function MyApp({ Component, pageProps }) {
+
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-T282LWR' });
-}, []);
-  return <Component {...pageProps} />
-  
+  }, []);
+  return (
+    <>
+      <Script
+        id="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+      });
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
