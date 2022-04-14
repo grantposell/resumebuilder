d05ce0476e1f0729d9787f5d0280de583c6f6617
grantposell <grantposell@gmail.com>
2022-04-14 18:22:06 -0500

    I think i finally fixed google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,30 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
 import Script from "next/script";
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-T282LWR' });
-  }, []);
   return (
     <>
       <Script
-        id="lazyOnload"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script id="lazyOnload">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-      });
-          `}
-      </Script>
+      <Script id="my-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
