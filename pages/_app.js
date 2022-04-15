af1ba8ed8cfa9c24f343a6d16c3ca1651d521f5c
grantposell <grantposell@gmail.com>
2022-04-14 20:51:31 -0500

    Added facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Script from "next/script";
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('XXXXXXXXXXXXXXXXXXXXX') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
 
   return (
     <>
