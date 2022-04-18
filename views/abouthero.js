c6c516440a1570a3955f33e80cca35cc47cef9b4
grantposell <grantposell@gmail.com>
2022-04-18 11:04:58 -0500

    Fixed the about me

--- views/abouthero.js
+++ views/abouthero.js
@@ -1,17 +1,22 @@
 export default function Abouthero() {
     return (
-        <div style={{margin: '100px'}}>
+        <div style={{ margin: '100px' }}>
             <h2 variant="h4" marked="center" align="left" component="h2">
-                About Us 
+                About Us
             </h2>
-            <p style={{textAlign:'justify'}}>
-            Finding a job is hard, and changing career paths is even more difficult. <br/><br/>
+            <p style={{ textAlign: 'justify' }}>
+                Finding a job is challenging and changing career paths is even more difficult.
 
-            The Founders of Raven Resumes started their career in Recruiting and Human Resources, where they worked with hiring managers from companies of all industries. Building rapport with these companies took a tailored approach, and often applicants applying to these jobs were not coached on how to apply to jobs and how to have the best shot at these jobs. This is why our founders took this knowledge, pivoted careers to gain data analytics skills, and built a platform for applicants.  <br/><br/>
+                <br /><br />
 
-            This is where Raven Resumes was born. <br/><br/>
+                {/* The Founders of Raven Resumes started their career in Recruiting and Human Resources, where they worked with hiring managers from companies of all industries. Building rapport with these companies took a tailored approach, and often applicants applying to these jobs were not coached on how to apply to jobs and how to have the best shot at these jobs. This is why our founders took this knowledge, pivoted careers to gain data analytics skills, and built a platform for applicants.  <br /><br /> */}
 
-            Raven Resumes is a service that is the applicant's advocate, someone that does the research for you and gives you the advice that you need to succeed in your job search. Raven Resumes gives you tools, tips, and advice on how to maneuver the tricky landscape that is job hunting. So check out our many services: resume editing, career mapping, interview principles, and application techniques.</p>
+                This is where Raven Resumes was born. <br /><br />
+
+                Raven Resumes is here to help eliminate some of the pressure and stress that comes with searching for a new job and, subsequently, a new career. Raven Resumes is a service that is the applicant's advocate, someone that does the research for you and gives you the advice that you need to succeed in your job search. We provide you with the tools, tips, latest industry updates, and advice on maneuvering the tricky landscape that is job hunting. If you're tired of having the system work you and ready to take charge of your life, check out our many services: resume editing, career mapping, interview principles, and application techniques.
+                {/* Raven Resumes is a service that is the applicant's advocate, someone that does the research for you and gives you the advice that you need to succeed in your job search. Raven Resumes gives you tools, tips, and advice on how to maneuver the tricky landscape that is job hunting. So check out our many services: resume editing, career mapping, interview principles, and application techniques. */}
+                
+                </p>
         </div>
     )
 }
